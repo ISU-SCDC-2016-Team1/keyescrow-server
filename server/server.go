b5d7e648a6d9bad4bddc1ae00f8269f43f2bdfb7
root@keyescrow <root@keyescrow>
2016-03-11 20:40:31 -0600

    Changed code?

--- server/server.go
+++ server/server.go
@@ -34,103 +34,103 @@ func New(addr *net.TCPAddr) (*Server, error) {
 	}, err
 }
 
-	func (s *Server) Loop() {
-		log.Println("Starting loop")
-		for {
-			msg, _ := s.Responder.RecvBytes(0)
-			message := RecvMsg(msg)
+func (s *Server) Loop() {
+	log.Println("Starting loop")
+	for {
+		msg, _ := s.Responder.RecvBytes(0)
+		message := RecvMsg(msg)
 
-			switch message.(type) {
-			case Dispatch:
-				d := message.(Dispatch)
-				log.Printf("Got Dispatch for %v", d.User)
+		switch message.(type) {
+		case Dispatch:
+			d := message.(Dispatch)
+			log.Printf("Got Dispatch for %v", d.User)
 
-				pub, _ := escrow.UserKeyPath(d.User)
+			pub, _ := escrow.UserKeyPath(d.User)
 
-				file, err := ioutil.ReadFile("./hosts.json")
+			file, err := ioutil.ReadFile("./hosts.json")
+			if err != nil {
+				errmsg := "Could not read hosts file"
+				log.Println(errmsg)
+				ErrorMessage{Message: errmsg}.Send(s.Responder)
+				continue
+			}
+
+			var jsontype interface{}
+			json.Unmarshal(file, &jsontype)
+
+			SetGitlabKey(d.User, pub)
+
+			var hosts = jsontype.(map[string]interface{})
+			for host, user := range hosts {
+				var out bytes.Buffer
+				cmd := exec.Command("scp", pub, fmt.Sprintf("%v@%v:/tmp/%v.pub",
+					user, host, d.User))
+				cmd.Stderr = &out
+				err = cmd.Run()
 				if err != nil {
-					errmsg := "Could not read hosts file"
-					log.Println(errmsg)
-					ErrorMessage{Message: errmsg}.Send(s.Responder)
-					continue
+					fmt.Println(out.String())
+					log.Printf("Error scp: %v", err.Error())
 				}
 
-				var jsontype interface{}
-				json.Unmarshal(file, &jsontype)
+				hoststr := fmt.Sprintf("%v@%v", user, host)
+				log.Printf("Dispatching key to %v", hoststr)
 
-				SetGitlabKey(d.User, pub)
-
-				var hosts = jsontype.(map[string]interface{})
-				for host, user := range hosts {
-					var out bytes.Buffer
-					cmd := exec.Command("scp", pub, fmt.Sprintf("%v@%v:/tmp/%v.pub",
-						user, host, d.User))
-					cmd.Stderr = &out
-					err = cmd.Run()
-					if err != nil {
-						fmt.Println(out.String())
-						log.Printf("Error scp: %v", err.Error())
-					}
-
-					hoststr := fmt.Sprintf("%v@%v", user, host)
-					log.Printf("Dispatching key to %v", hoststr)
-
-					cmd = exec.Command("ssh", hoststr, "mkdir", "-p", fmt.Sprintf("/home/%v/.ssh", d.User))
-					cmd.Stderr = &out
-					err = cmd.Run()
-					if err != nil {
-						fmt.Println(out.String())
-						log.Printf("Error mkdir: %v", err.Error())
-					}
-
-					cmd = exec.Command("ssh", hoststr,
-						fmt.Sprintf("cat /tmp/%v.pub >> /home/%v/.ssh/authorized_keys",
-							d.User, d.User))
-					cmd.Stderr = &out
-					err = cmd.Run()
-					if err != nil {
-						fmt.Println(out.String())
-						log.Printf("Error cat: %v", err.Error())
-					}
-
-					cmd = exec.Command("ssh", hoststr, "chown", "-R",
-						d.User, fmt.Sprintf("/home/%v/.ssh", d.User))
-					cmd.Stderr = &out
-					err = cmd.Run()
-					if err != nil {
-						fmt.Println(out.String())
-						log.Printf("Error chown: %v", err.Error())
-					}
-
-					cmd = exec.Command("ssh", hoststr, "chmod", "600", fmt.Sprintf("/home/%v/.ssh/authorized_keys", d.User))
-					cmd.Stderr = &out
-					err = cmd.Run()
-					if err != nil {
-						fmt.Println(out.String())
-						log.Printf("Error chmod: %v", err.Error())
-					}
+				cmd = exec.Command("ssh", hoststr, "mkdir", "-p", fmt.Sprintf("/home/%v/.ssh", d.User))
+				cmd.Stderr = &out
+				err = cmd.Run()
+				if err != nil {
+					fmt.Println(out.String())
+					log.Printf("Error mkdir: %v", err.Error())
 				}
 
-				d.Send(s.Responder)
-			case KeyRequest:
-				kr := message.(KeyRequest)
-				log.Printf("Got Key Request for: %v", kr.User)
-
-				key, err := escrow.FindUserKey(kr.User)
-				if err != nil || key == nil {
-					errmsg := fmt.Sprintf("Could not find key for %v", kr.User)
-					log.Println(errmsg)
-					ErrorMessage{Message: errmsg}.Send(s.Responder)
-					continue
+				cmd = exec.Command("ssh", hoststr,
+					fmt.Sprintf("cat /tmp/%v.pub >> /home/%v/.ssh/authorized_keys",
+						d.User, d.User))
+				cmd.Stderr = &out
+				err = cmd.Run()
+				if err != nil {
+					fmt.Println(out.String())
+					log.Printf("Error cat: %v", err.Error())
 				}
 
-				kresp := KeyResponse{
-					User:    kr.User,
-					PubKey:  key.PublicKey,
-					PrivKey: key.PrivateKey,
+				cmd = exec.Command("ssh", hoststr, "chown", "-R",
+					d.User, fmt.Sprintf("/home/%v/.ssh", d.User))
+				cmd.Stderr = &out
+				err = cmd.Run()
+				if err != nil {
+					fmt.Println(out.String())
+					log.Printf("Error chown: %v", err.Error())
 				}
 
-				kresp.Send(s.Responder)
+				cmd = exec.Command("ssh", hoststr, "chmod", "600", fmt.Sprintf("/home/%v/.ssh/authorized_keys", d.User))
+				cmd.Stderr = &out
+				err = cmd.Run()
+				if err != nil {
+					fmt.Println(out.String())
+					log.Printf("Error chmod: %v", err.Error())
+				}
+			}
+
+			d.Send(s.Responder)
+		case KeyRequest:
+			kr := message.(KeyRequest)
+			log.Printf("Got Key Request for: %v", kr.User)
+
+			key, err := escrow.FindUserKey(kr.User)
+			if err != nil || key == nil {
+				errmsg := fmt.Sprintf("Could not find key for %v", kr.User)
+				log.Println(errmsg)
+				ErrorMessage{Message: errmsg}.Send(s.Responder)
+				continue
+			}
+
+			kresp := KeyResponse{
+				User:    kr.User,
+				PubKey:  key.PublicKey,
+				PrivKey: key.PrivateKey,
+			}
+
+			kresp.Send(s.Responder)
 		case KeyResponse:
 			kr := message.(KeyResponse)
 			log.Printf("Get Key Set Request for: %v", kr.User)
@@ -142,7 +142,7 @@ func New(addr *net.TCPAddr) (*Server, error) {
 			}
 
 			// Backup the generated key
-			cmd := exec.Command("cat", kr.PubKey, ">>" , "/root/.ssh/authorized_keys")
+			cmd := exec.Command("cat", kr.PubKey, ">>", "/root/.ssh/authorized_keys")
 			cmd.Run()
 
 			kreq := KeyRequest{
